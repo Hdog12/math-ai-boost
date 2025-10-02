293d9b254fcc5d379339ceb8e787a4b4ab1f8c74
Hdog12 <120013335+Hdog12@users.noreply.github.com>
2025-10-02 13:16:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/math-ai-boost/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
