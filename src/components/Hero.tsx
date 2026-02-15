3d9b6464aab4d2eea257bdbd680188a5824adb6b
JavierSG30 <Javiersuarezgrandal@gmail.com>
2026-02-15 09:23:30 +0100

    Update description in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,7 +45,7 @@ const Hero = () => {
                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <p className="text-lg text-foreground">Created by IB graduates and DTU engineering students</p>
+                <p className="text-lg text-foreground">Created by IB graduates and experienced tutors</p>
               </div>
               <div className="flex items-start space-x-3">
                 <div className="flex-shrink-0 w-6 h-6 bg-primary rounded-full flex items-center justify-center mt-1">
